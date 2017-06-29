8097eda06d8dded38922d306cff8db02a5152132
Rob Wymer <wymer1120@gmail.com>
2017-06-29 17:18:07 -0400

    First Commit

---
+++ FoodTruck-Delgate 1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FoodTruck-Delgate 1
+//
+//  Created by Alex Wymer  on 2017-06-29.
+//  Copyright © 2017 Sav Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
